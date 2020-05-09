92039e2afe40eee4d0be28cca3192e2a56303dd6
dvidsilva <contacto@dvidsilva.com>
2020-05-08 21:42:19 -0400

    more plugins

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,19 +1,27 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import LogoSquared from '@images/logo-squared.png'
+import Styles from './header.module.scss';
 
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#fabf67`,
       marginBottom: `1.45rem`,
     }}
+    className={ Styles.container }
   >
     <div
       style={{
         margin: `0 auto`,
         maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
+        backgroundImage: `url(${LogoSquared})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: '85px',
+        backgroundPosition: '10px 5px',
+        paddingLeft: '120px',
       }}
     >
       <h1 style={{ margin: 0 }}>
@@ -24,6 +32,7 @@ const Header = ({ siteTitle }) => (
             textDecoration: `none`,
           }}
         >
+          {/* <img src={ LogoSquared } alt="Techqueria logo" className={ Styles.logoImg } /> */}
           {siteTitle}
         </Link>
       </h1>
