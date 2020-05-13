35e96f4e8ec16d8ee6ba0a5cc6804436414d467d
dvidsilva <contacto@dvidsilva.com>
2020-05-12 23:35:43 -0400

    generating pages from json

--- src/templates/Episode.js
+++ src/templates/Episode.js
@@ -3,7 +3,7 @@ import Layout from "../components/layout"
 
 const EpisodeTemplate = (props) => (
   <Layout>
-    <h1>{ props.title || 'Page Template' }</h1>
+    <h1>{ props.pathContext.title || 'Page Template' }</h1>
   </Layout>
 );
 
