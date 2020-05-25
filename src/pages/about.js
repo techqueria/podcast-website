a96c073b108ae3ef1707b1d7763f8360c3196689
dvidsilva <contacto@dvidsilva.com>
2020-05-25 19:25:16 -0400

    cleaned up index page

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,14 +5,17 @@ import SEO from "../components/seo";
 const AboutPage = () => (
   <Layout>
     <SEO title="Techqueria Podcast: About" />
-    <h1>About</h1>
-    <h2>Techqueria</h2>
+    <h1>Techqueria podcast</h1>
+    <p>Techqueria and Points of Presence Media have come together to create a podcast featuring in-depth narrative stories, interviews and business advice about and from the perspective of the Latinx tech community. With a huge need to tell our stories properly and with necessary journalistic context, we are here to make a difference and a lot of noise in this growing medium!</p>
+    <p><strong>The first episode of the Techqueria podcast is coming soon!</strong></p>
+    <p>San Francisco Bay Area tech and culture reporter Jose Fermoso introduces the types of stories that will be featured on the show. Look for conversations and news context on socioeconomic displacement, immigration policies and multigenerational tech and business startups, as well as the latest on the effect of the coronavirus pandemic on the Latinx community. Get ready!</p>
+    <h2>About</h2>
+    <h3>Techqueria</h3>
     <p>Techqueria is a 501c3 nonprofit that empowers Latinx professionals with the resources and support that they need to thrive and become leaders in the tech industry.</p>
     <p>To that end, we work with both tech companies and employee resource groups (ERGs) to build Latinx-centered spaces that revolve around career advice, technical talks, mentorship, open jobs, networking events, speaking opportunities, and open-source in order to comprehensively affect change in the tech industry.</p>
     <p>Coming from all walks of life, we believe that the diversity of our community is the most reliable asset we have. Our space aims to be inclusive so we invite Latinx from the regions of the Caribbean, Haiti, and Brazil as well as those who identify as Afro-Latinx, Asian-Latinx or LGBTQIA. The term Latinx is used instead of Latino or Latina because it is a gender-neutral and inclusive term.</p>
 
-    <h2>Points of Presence</h2>
-
+    <h3>Points of Presence</h3>
     <p>Points of Presence Media is a news company focused on telling true stories of underrepresented people making, using, and living with technology. In a rush to understand the fast-paced, digital nature of the modern world, mainstream media has relied on transactional stories to the detriment of those that connect with regular people on a personal level. Points of Presence is changing that. A team of reporters, editors, technologists, and educators has come together to tell new stories through untraditional approaches. Find out more about them here.</p>
 
   </Layout>
